16d6b976752322c531ae63eda02f107c2e366f94
alexsilva9913 <alexsilva9913@gmail.com>
2021-02-26 15:29:24 +0000

    Branch 1.2

--- app/src/main/java/com/example/githubteste/MainActivity.kt
+++ app/src/main/java/com/example/githubteste/MainActivity.kt
@@ -24,6 +24,8 @@ class MainActivity : AppCompatActivity() {
 
     //New Branch
 
+    //2
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
